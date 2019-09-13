a4178fdaa0142085d2de49784be81f45a3091233
Asheesh Laroia <asheesh@reduct.video>
2019-09-13 09:27:29 -0700

    Add Reduct Tables

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MaterialTable from "material-table";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    fetch("/columns.json")
+      .then(response => response.json())
+      .then(columns => this.setState({ columns }));
+    fetch("/data.json")
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
+  render() {
+    return (
+      <div style={{ maxWidth: "100%" }}>
+        <MaterialTable
+          columns={this.state.columns}
+          data={this.state.data}
+          title="Reduct Data"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
